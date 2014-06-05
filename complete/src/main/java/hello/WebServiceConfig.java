e9efbf817023bb858a69cb3d7c9ee2e64320d9cf
Greg Turnquist <gturnquist@gopivotal.com>
2014-06-05 09:41:43 -0500

    Update to Spring WS 2.2.0.RELEASE and make appropriate edits
    * Usage @EnableWs, but still need dispatcherServlet to override Boot's usage of dispatcherServlet
    * Made mods to text to better explain bits and pieces.
    * Removed milestone/snapshot repos from build files
    * Confirmed final version properly generates expected SOAP message

--- complete/src/main/java/hello/WebServiceConfig.java
+++ complete/src/main/java/hello/WebServiceConfig.java
@@ -7,25 +7,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.server.endpoint.adapter.DefaultMethodEndpointAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+@EnableWs
 @Configuration
-public class WebServiceConfig {
-    @Bean(name = "ws")
-    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+public class WebServiceConfig extends WsConfigurerAdapter {
+    @Bean
+    public ServletRegistrationBean dispatcherServlet(ApplicationContext applicationContext) {
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();
         servlet.setApplicationContext(applicationContext);
 
         return new ServletRegistrationBean(servlet, "/ws/*");
-    }
-
-    @Bean
-    public DefaultMethodEndpointAdapter defaultMethodEndpointAdapter() {
-        return new DefaultMethodEndpointAdapter();
     }
 
     @Bean(name = "countries")
