c225f4ea207db326e88b8b72ca768565f462f2a1
Greg Turnquist <gturnquist@gopivotal.com>
2014-07-10 10:39:23 -0500

    Update to use a few more standard conventions
    * Plug in servlet.setTransformWsdlLocations(true) to let soap:address adjust dynamically
    * Plug in elementFormDefault="qualified" to the XSD
    * Fix the namespace in request.xml
    * Use "gs" namespace in request.xml

--- complete/src/main/java/hello/CountryRepository.java
+++ complete/src/main/java/hello/CountryRepository.java
@@ -1,56 +1,56 @@
 package hello;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
 import io.spring.guides.gs_producing_web_service.Country;
 import io.spring.guides.gs_producing_web_service.Currency;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
-
 @Component
 public class CountryRepository {
-    private static final List<Country> countries = new ArrayList<Country>();
+	private static final List<Country> countries = new ArrayList<Country>();
 
-    @PostConstruct
-    public void initData() {
-        Country spain = new Country();
-        spain.setName("Spain");
-        spain.setCapital("Madrid");
-        spain.setCurrency(Currency.EUR);
-        spain.setPopulation(46704314);
+	@PostConstruct
+	public void initData() {
+		Country spain = new Country();
+		spain.setName("Spain");
+		spain.setCapital("Madrid");
+		spain.setCurrency(Currency.EUR);
+		spain.setPopulation(46704314);
 
-        countries.add(spain);
+		countries.add(spain);
 
-        Country poland = new Country();
-        poland.setName("Poland");
-        poland.setCapital("Warsaw");
-        poland.setCurrency(Currency.PLN);
-        poland.setPopulation(38186860);
+		Country poland = new Country();
+		poland.setName("Poland");
+		poland.setCapital("Warsaw");
+		poland.setCurrency(Currency.PLN);
+		poland.setPopulation(38186860);
 
-        countries.add(poland);
+		countries.add(poland);
 
-        Country uk = new Country();
-        uk.setName("United Kingdom");
-        uk.setCapital("London");
-        uk.setCurrency(Currency.GBP);
-        uk.setPopulation(63705000);
+		Country uk = new Country();
+		uk.setName("United Kingdom");
+		uk.setCapital("London");
+		uk.setCurrency(Currency.GBP);
+		uk.setPopulation(63705000);
 
-        countries.add(uk);
-    }
+		countries.add(uk);
+	}
 
-    public Country findCountry(String name) {
-        Assert.notNull(name);
+	public Country findCountry(String name) {
+		Assert.notNull(name);
 
-        Country result = null;
+		Country result = null;
 
-        for (Country country : countries) {
-            if (name.equals(country.getName())) {
-                result = country;
-            }
-        }
+		for (Country country : countries) {
+			if (name.equals(country.getName())) {
+				result = country;
+			}
+		}
 
-        return result;
-    }
+		return result;
+	}
 }
--- complete/src/main/java/hello/WebServiceConfig.java
+++ complete/src/main/java/hello/WebServiceConfig.java
@@ -4,12 +4,9 @@ import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
-import org.springframework.ws.server.endpoint.adapter.DefaultMethodEndpointAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
@@ -18,26 +15,26 @@ import org.springframework.xml.xsd.XsdSchema;
 @EnableWs
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
-    @Bean
-    public ServletRegistrationBean dispatcherServlet(ApplicationContext applicationContext) {
-        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
-        servlet.setApplicationContext(applicationContext);
+	@Bean
+	public ServletRegistrationBean dispatcherServlet(ApplicationContext applicationContext) {
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		servlet.setApplicationContext(applicationContext);
+		servlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean(servlet, "/ws/*");
+	}
 
-        return new ServletRegistrationBean(servlet, "/ws/*");
-    }
+	@Bean(name = "countries")
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
+		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+		wsdl11Definition.setPortTypeName("CountriesPort");
+		wsdl11Definition.setLocationUri("/ws");
+		wsdl11Definition.setTargetNamespace("http://spring.io/guides/gs-producing-web-service");
+		wsdl11Definition.setSchema(countriesSchema);
+		return wsdl11Definition;
+	}
 
-    @Bean(name = "countries")
-    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
-        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
-        wsdl11Definition.setPortTypeName("CountriesPort");
-        wsdl11Definition.setLocationUri("/countries/");
-        wsdl11Definition.setTargetNamespace("http://spring.io/guides/gs-producing-web-service");
-        wsdl11Definition.setSchema(countriesSchema);
-        return wsdl11Definition;
-    }
-
-    @Bean
-    public XsdSchema countriesSchema() {
-        return new SimpleXsdSchema(new ClassPathResource("countries.xsd"));
-    }
+	@Bean
+	public XsdSchema countriesSchema() {
+		return new SimpleXsdSchema(new ClassPathResource("countries.xsd"));
+	}
 }
